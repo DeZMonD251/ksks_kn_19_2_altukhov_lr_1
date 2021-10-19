faf564c33ccfa80d3bf6c8cee00f708d4bf14bce
Dezmond <stalkerprizrak111@gmail.com>
2021-10-19 10:52:04 +0300

    1

---
+++ Serv/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Serv
+{
+    public struct Received
+    {
+        public IPEndPoint Sender;
+        public string Message;
+    }
+
+    abstract class UdpBase
+    {
+        protected UdpClient Client;
+
+        protected UdpBase()
+        {
+            Client = new UdpClient();
+        }
+
+        public async Task<Received> Receive()
+        {
+            var result = await Client.ReceiveAsync();
+            return new Received()
+            {
+                Message = Encoding.ASCII.GetString(result.Buffer, 0, result.Buffer.Length),
+                Sender = result.RemoteEndPoint
+            };
+        }
+    }
+
+    //Server
+    class UdpListener : UdpBase
+    {
+        private IPEndPoint _listenOn;
+        public UdpListener() : this(new IPEndPoint(IPAddress.Any, 32123))
+        {
+        }
+        public UdpListener(IPEndPoint endpoint)
+        {
+            _listenOn = endpoint;
+            Client = new UdpClient(_listenOn);
+        }
+        public void Reply(string message, IPEndPoint endpoint)
+        {
+            var datagram = Encoding.ASCII.GetBytes(message);
+            Client.Send(datagram, datagram.Length, endpoint);
+        }
+    }
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //create a new server
+            var server = new UdpListener();
+
+            //start listening for messages and copy the messages back to the client
+            Task.Factory.StartNew(async () => {
+                while (true)
+                {
+                    var received = await server.Receive();
+                    server.Reply("copy " + received.Message, received.Sender);
+                    if (received.Message == "quit")
+                        break;
+                }
+            });
+        }
+    }
+}
